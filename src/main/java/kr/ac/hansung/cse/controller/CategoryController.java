9f5c22af899ffd70b0c1e042be9adff1691e3f68
lhd <ehd001111@naver.com>
2026-04-19 23:31:20 +0900

    feat: 카테고리 관리 및 상품 검색 요청 처리를 위한 Controller 수정

--- src/main/java/kr/ac/hansung/cse/controller/CategoryController.java
+++ src/main/java/kr/ac/hansung/cse/controller/CategoryController.java
@@ -1,38 +1,60 @@
 package kr.ac.hansung.cse.controller;
 
 import jakarta.validation.Valid;
-import kr.ac.hansung.cse.model.Category;
+import kr.ac.hansung.cse.exception.DuplicateCategoryException;
 import kr.ac.hansung.cse.model.CategoryForm;
-import kr.ac.hansung.cse.service.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
+import kr.ac.hansung.cse.service.CategoryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/categories")
 public class CategoryController {
 
-    @Autowired
-    private ProductService productService;
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public String listCategories(Model model) {
+        model.addAttribute("categories", categoryService.getAllCategories());
+        return "categoryList";
+    }
 
     @GetMapping("/create")
     public String showCreateForm(Model model) {
         model.addAttribute("categoryForm", new CategoryForm());
-        return "categoryCreateForm";
+        return "categoryForm";
     }
 
     @PostMapping("/create")
-    public String createCategory(@Valid CategoryForm categoryForm, BindingResult result) {
-        if (result.hasErrors()) {
-            return "categoryCreateForm";
+    public String createCategory(@Valid @ModelAttribute CategoryForm categoryForm, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            return "categoryForm";
         }
-        Category category = new Category();
-        category.setName(categoryForm.getName());
-        productService.saveCategory(category);
-        return "redirect:/products";
+        try {
+            categoryService.createCategory(categoryForm.getName());
+            redirectAttributes.addFlashAttribute("successMessage", "카테고리 등록 완료");
+        } catch (DuplicateCategoryException e) {
+            bindingResult.rejectValue("name", "duplicate", e.getMessage());
+            return "categoryForm";
+        }
+        return "redirect:/categories";
+    }
+
+    @PostMapping("/{id}/delete")
+    public String deleteCategory(@PathVariable Long id, RedirectAttributes redirectAttributes) {
+        try {
+            categoryService.deleteCategory(id);
+            redirectAttributes.addFlashAttribute("successMessage", "카테고리 삭제 완료");
+        } catch (IllegalStateException e) {
+            redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
+        }
+        return "redirect:/categories";
     }
 }
